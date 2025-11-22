f90dfc751edeff008af85a700cccc4b5e5db28f6
kyahwill <knownbutnotloved@gmail.com>
2025-11-22 08:44:30 +0800

    fix: Fixed react's issue with authlayout

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/lib/contexts/ThemeContext";
 import { ThemeLoader } from "@/components/theme-loader";
+import { AuthProvider } from "@/lib/contexts/auth-context";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,10 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>
-          <ThemeLoader />
-          {children}
-        </ThemeProvider>
+        <AuthProvider>
+          <ThemeProvider>
+            <ThemeLoader />
+            {children}
+          </ThemeProvider>
+        </AuthProvider>
       </body>
     </html>
   );
